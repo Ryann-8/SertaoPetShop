98acc8ab99d3730dfb10e2c3e0be7a125029b074
Ryann-8 <ryangabri306@gmail.com>
2025-10-22 17:35:42 -0300

    .

---
+++ petshop.py
@@ -0,0 +1,24 @@
+usuarios = []
+adm = []
+op = 99
+s_n = input('Olá, seja bem vindo, você ja possui cadastro?[s/n] ')
+if s_n == 'n':
+    while op != 0:
+        print('------Cadastro------')
+        print('0-Sair para o Menu')
+        print('1-Cadastrar administrador')
+        print('2-Cadastrar usuário')
+        print('--------------------')
+        op = int(input('digite a opção desejada'))
+        if op == 1:
+            login= input('digite um login: ')
+            senha = input('digite uma senha: ')
+            adm.append([login,senha])
+            print('Cadastro concluído')
+        elif op == 2:
+            login = input('digite um login: ')
+            senha = input('digite uma senha: ')
+            usuarios.append([login,senha])
+            print('Cadastro concluído')
+elif s_n == 's':
+    #fazer o menu principal
