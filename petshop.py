be0ea4292450866e08b1dfd208a874b4c15cc5f4
Ryann-8 <ryangabri306@gmail.com>
2025-10-24 10:38:41 -0300

    falta fazer o gerenciamento de serviços

--- petshop.py
+++ petshop.py
@@ -1,51 +1,122 @@
 usuarios = [['ryancliente','cliente123']]
 adm = [['ryanadm','adm123']]
-produtos = [["Ração para cães"],["Brinquedo de borracha"],["Coleira ajustável"],["Shampoo para pets"],["Caminha confortável"]]
-servicos = [["Banho e tosa"],["Consulta veterinária"],["Hospedagem de pets"],["Adestramento"],["Vacinação"]]
-
-
-print('-----Menu de login-----')
-print('1-Fazer login como usuário')
-print('2-Fazer login como administrador')
-print('-----------------------')
-op = int(input('Digite a opção desejada: '))
-if op == 1:
-    login = input('Login: ')
-    senha = input('Senha: ')
-    confirm = []
-    confirm.insert(0 ,[login,senha])
-    if confirm[0][0] == usuarios[0][0] and confirm[0][1] == usuarios[0][1]:
-        print(f'Seja bem vindo, {login}')
-    else:
-        print('Você ainda não possui um login, cadastre-se agora:')
-        login = input('Crie um login: ')
-        senha = input('crie uma senha: ')
-        usuarios.insert(0, [login,senha])
-    while op != 0:
-        print('---Menu Principal---')
-        print('1-Produtos a venda')
-        print('2-Agendar serviços')
-        print('0-Finalizar')
-        print('--------------------')
-        op = int(input('O que deseja: '))
-        if op == 1:
-            ind = 0
-            for i in produtos:
-                ind += 1
-                print('-------------')
-                print(f'{ind}-{i[0]}')
-            print('-------------')
-
-if op == 2:
-    login = input('Login: ')
-    senha = input('Senha: ')
-    confirm = []
-    confirm.insert(0, [login, senha])
-    if confirm[0][0] == adm[0][0] and confirm[0][1] == adm[0][1]:
-        print(f'Seja bem vindo, {login}')
-    else:
-        print('Você ainda não possui um login, cadastre-se agora:')
-        login = input('Crie um login: ')
-        senha = input('crie uma senha: ')
-        usuarios.insert(0, [login,senha])
-
+produtos = [["Ração para cães", "R$60",],["Brinquedo de borracha", "R$60"],["Coleira ajustável", "R$60"],["Shampoo para pets", "R$60"],["Caminha confortável", "R$60"]]
+servicos = [["Banho e tosa", "R$100", "Às 10H"],["Consulta veterinária", "R$100", "Às 10H"],["Hospedagem de pets", "R$100", "Às 10H"],["Adestramento", "R$100", "Às 10H"],["Vacinação", "R$100", "Às 10H"]]
+while True:
+    print('-----Menu de login-----')
+    print('0-Finalizar')
+    print('1-Fazer login como usuário')
+    print('2-Fazer login como administrador')
+    print('-----------------------')
+    op = int(input('Digite a opção desejada: '))
+    if op == 0:
+        break
+    elif op == 1:
+        login = input('Login: ')
+        senha = input('Senha: ')
+        confirm = []
+        confirm.insert(0 ,[login,senha])
+        if confirm[0][0] == usuarios[0][0] and confirm[0][1] == usuarios[0][1]:
+            print(f'Seja bem vindo, {login}')
+        else:
+            print('Você ainda não possui um login, cadastre-se agora:')
+            login = input('Crie um login: ')
+            senha = input('crie uma senha: ')
+            usuarios.insert(0, [login,senha])
+        while op != 0:
+            print('---Menu Principal---')
+            print('1-Produtos a venda')
+            print('2-Agendar serviços')
+            print('0-Sair')
+            print('--------------------')
+            op = int(input('O que deseja: '))
+            if op == 1:
+                ind = 0
+                for i in produtos:
+                    ind += 1
+                    print('-------------')
+                    print(f'{ind}-{i[0]} {i[1]}')
+                ind = int(input('O que deseja comprar? '))
+                pg = input('Qual será a forma de pagamento? ')
+                print('Pagamento confirmado, obrigado pela preferência e volte sempre')
+                produtos.pop(ind-1)
+            elif op == 2:
+                ind = 0
+                for i in servicos:
+                    ind += 1
+                    print('-------------')
+                    print(f'{ind}-{i[0]} {i[2]} {i[1]}')
+                ind = int(input('O que deseja comprar? '))
+                pg = input('Qual será a forma de pagamento? ')
+                print('Pagamento confirmado, obrigado pela preferência e volte sempre')
+                servicos.pop(ind - 1)
+    if op == 2:
+        login = input('Login: ')
+        senha = input('Senha: ')
+        confirm = []
+        confirm.insert(0, [login, senha])
+        if confirm[0][0] == adm[0][0] and confirm[0][1] == adm[0][1]:
+            print(f'Seja bem vindo, {login}')
+        else:
+            print('Você ainda não possui um login, cadastre-se agora:')
+            login = input('Crie um login: ')
+            senha = input('crie uma senha: ')
+            usuarios.insert(0, [login,senha])
+        while op !=0:
+            print('---Menu Principal---')
+            print('1-Gerenciar produtos a venda')
+            print('2-Gerenciar serviços')
+            print('0-Sair')
+            print('--------------------')
+            op = int(input('O que deseja: '))
+            if op == 1:
+                print('---Produtos a venda---')
+                ind = 1
+                for i in produtos:
+                    print(f'{ind}-{i[0]} {i[1]}')
+                    ind += 1
+                while op != 0:
+                    print('---------------')
+                    print('0-sair')
+                    print('1-Remover item')
+                    print('2-Alterar item')
+                    print('3-Adiconar item')
+                    print('4-listar itens')
+                    op = int(input('Ação desejada: '))
+                    ind = 1
+                    if op == 1:
+                        ind = int(input('Digite o índice do item que deseja remover: '))
+                        produtos.pop(ind-1)
+                        print('Item removido')
+                    elif op == 2:
+                        ind = int(input('Digite o índice do item que deseja alterar: '))
+                        a = input('deseja alterar o nome ou o preço?[n/p] ')
+                        if a == 'n':
+                            alt = input('Digite o novo nome do item: ')
+                            produtos[ind-1][0] = alt
+                            print('Nome alterado')
+                        elif a == 'p':
+                            alt = input('Digite o novo valor do item: ')
+                            produtos[ind-1][1] = alt
+                    elif op == 3:
+                        ad_n = input('Digite o nome do novo item: ')
+                        ad_p = input('Digite o preço do novo item: ')
+                        produtos.append([ad_n,ad_p])
+                        print('Produto adicionado')
+                    elif op == 4:
+                        for i in produtos:
+                            print(f'{ind}-{i[0]} {i[1]}')
+                            ind += 1
+            elif op ==2:
+                print('---Serviços disponíveis---')
+                ind = 1
+                for i in servicos:
+                    print(f'{ind}-{i[0]}, {i[2]} {i[1]}')
+                while op != 0:
+                    print('---------------')
+                    print('0-sair')
+                    print('1-Remover serviço')
+                    print('2-Alterar serviço')
+                    print('3-Adiconar serviço')
+                    print('4-listar serviços')
+                    op = int(input('Ação desejada: '))
