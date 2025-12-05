1b2f2fb25476bc9df80a5d103aaa01ef7b8007ce
Ryann-8 <ryangabri306@gmail.com>
2025-12-05 13:52:34 -0300

    fazendo funcoes

--- funcoes_pet.py
+++ funcoes_pet.py
@@ -1,9 +1,3 @@
-usuarios = [['ryancliente','cliente123']]
-adm = [['ryanadm','adm123']]
-produtos = [["Ração para cães", "R$60", 5],["Brinquedo de borracha", "R$60", 5],["Coleira ajustável", "R$60", 5],["Shampoo para pets", "R$60", 5],["Caminha confortável", "R$60", 5]]
-servicos = [["Banho e tosa", "R$100", ["Às 10H", "Às 14H"]],["Consulta veterinária", "R$100", ["Às 10H", "Às 14H"]],["Hospedagem de pets", "R$100", ["Às 10H", "Às 14H"]],["Adestramento", "R$100", ["Às 10H", "Às 14H"]],["Vacinação", "R$100", ["Às 10H", "Às 14H"]]]
-
-
 def menu(tipo):
     if tipo == 'login':
         print('-----Menu de login-----')
@@ -60,17 +54,17 @@ def criar(tipo):
         else:
             print('Login existente ou inválido, tente outro')
 
-def listar(tipo):
-    if tipo == 'produtos':
+def listar(tipo, ti):
+    if ti == 'produtos':
         ind = 0
-        for i in produtos:
+        for i in tipo:
             ind += 1
             print('-------------')
             print(f'{ind}-{i[0]} {i[1]}')
             print(f'Estoque: {i[2]}')
-    elif tipo == 'servicos':
+    elif ti == 'servicos':
         ind = 0
-        for i in servicos:
+        for i in tipo:
             ind += 1
             print('-------------')
             print(f'{ind}-{i[0]} {i[1]}:')
@@ -79,7 +73,7 @@ def listar(tipo):
                 print(f'{hr}-{a}')
                 hr += 1
 
-def comprar(qtde, ind):
+def comprar(qtde, ind, produtos):
     if qtde <= 0:
         print('Valor inválido')
     elif produtos[ind - 1][2] < qtde:
@@ -89,7 +83,62 @@ def comprar(qtde, ind):
         print('Pagamento confirmado, obrigado pela preferência e volte sempre')
         produtos[ind - 1][2] -= qtde
 
-def agendar(ind, hr):
-    pg = input('Qual será a forma de pagamento? ')
-    print('Atendimento agendado')
-    servicos[ind - 1][2].pop(hr - 1)
+def agendar(ind, hr, servicos):
+    if hr <= len(servicos[ind][2]) and hr > 0:
+        pg = input('Qual será a forma de pagamento? ')
+        print('Atendimento agendado')
+        servicos[ind - 1][2].pop(hr - 1)
+    else:
+        print('Valor inválido, tente de novo')
+
+def remover(tipo):
+    print('1-Remover do estoque')
+    print('2-Remover produto por completo')
+    remo = int(input('Ação desejada: '))
+    if remo == 1:
+        ind = int(input('Digite o índice do item que deseja remover estoque: '))
+        est = int(input('Quantidade que será a removida do item: '))
+        if 0 < est <= tipo[ind - 1][2]:
+            tipo[ind - 1][2] -= est
+            print('Estoque atualizado')
+        else:
+            print('Valor invalido (Negativo ou naior que o estoque preexistente)')
+    elif remo == 2:
+        ind = int(input('Digite o índice do item que deseja remover: '))
+        tipo.pop(ind - 1)
+        print('Item removido')
+
+def alterar(tipo):
+    ind = int(input('Digite o índice do item que deseja alterar: '))
+    a = input('deseja alterar o nome ou o preço?[n/p] ')
+    if a == 'n':
+        alt = input('Digite o novo nome do item: ')
+        tipo[ind - 1][0] = alt
+        print('Nome alterado')
+    elif a == 'p':
+        alt = input('Digite o novo valor do item: ')
+        if '-' not in alt:
+            tipo[ind - 1][1] = alt
+        else:
+            print('Valor inválido (Não pode conter - no valor)')
+
+def adicionar(tipo):
+    print('1-Adicionar estoque de um item que já está no catálogo')
+    print('2-Adicionar novo item')
+    ad = int(input('Ação desejada: '))
+    if ad == 1:
+        ind = int(input('Digite o índice do item que deseja adicionar estoque: '))
+        est = int(input('Quantidade que será a dicionada ao item: '))
+        if est > 0:
+            tipo[ind - 1][2] += est
+        else:
+            print('Quantidade inválida')
+    elif ad == 2:
+        ad_n = input('Digite o nome do novo item: ')
+        ad_p = input('Digite o preço do novo item: ')
+        ad_e = int(input('Quanto será o estoque desse item: '))
+        if len(ad_n.strip()) != 0 and len(ad_p.strip()) != 0 and ad_e > 0:
+            tipo.append([ad_n, ad_p, ad_e])
+            print('Produto adicionado')
+        else:
+            print('Valores inválidos')
--- petshop.py
+++ petshop.py
@@ -1,8 +1,17 @@
 import funcoes_pet as pet
-from funcoes_pet import usuarios
-from funcoes_pet import adm
-from funcoes_pet import produtos
-from funcoes_pet import servicos
+usuarios = [['ryancliente','cliente123']]
+adm = [['ryanadm','adm123']]
+produtos = [["Ração para cães", "R$60", 5],
+            ["Brinquedo de borracha", "R$60", 5],
+            ["Coleira ajustável", "R$60", 5],
+            ["Shampoo para pets", "R$60", 5],
+            ["Caminha confortável", "R$60", 5]]
+
+servicos = [["Banho e tosa", "R$100", ["Às 10H", "Às 14H"]],
+            ["Consulta veterinária", "R$100", ["Às 10H", "Às 14H"]],
+            ["Hospedagem de pets", "R$100", ["Às 10H", "Às 14H"]],
+            ["Adestramento", "R$100", ["Às 10H", "Às 14H"]],
+            ["Vacinação", "R$100", ["Às 10H", "Às 14H"]]]
 while True:
     op = pet.menu('login')
     if op == 0:
@@ -14,15 +23,15 @@ while True:
         while op != 0:
             op = pet.menu('usuario')
             if op == 1:
-                pet.listar('produtos')
+                pet.listar(produtos, 'produtos')
                 ind = int(input('O que deseja comprar? '))
                 qtde = int(input('Quantos você deseja comprar?'))
-                pet.comprar(qtde, ind)
+                pet.comprar(qtde, ind, produtos)
             elif op == 2:
-                pet.listar('servicos')
+                pet.listar(servicos, 'servicos')
                 ind = int(input('O que deseja marcar? '))
                 hr = int(input('Qual horário deseja? '))
-                pet.agendar(ind, hr)
+                pet.agendar(ind, hr, servicos)
 
     elif op == 2:
         confirm = pet.confirmar(adm)
@@ -32,7 +41,7 @@ while True:
                 pet.menu('adm')
                 if op == '1':
                     print('---Produtos a venda---')
-                    pet.listar('produtos')
+                    pet.listar(produtos, 'produtos')
                     while op != 0:
                         print('---------------')
                         print('0-sair')
@@ -43,60 +52,13 @@ while True:
                         op = int(input('Ação desejada: '))
                         ind = 1
                         if op == 1:
-                            print('1-Remover do estoque')
-                            print('2-Remover produto por completo')
-                            remo = int(input('Ação desejada: '))
-                            if remo == 1:
-                                ind = int(input('Digite o índice do item que deseja remover estoque: '))
-                                est = int(input('Quantidade que será a removida do item: '))
-                                if 0 < est <= produtos[ind-1][2]:
-                                    produtos[ind-1][2] -= est
-                                    print('Estoque atualizado')
-                                else:
-                                    print('Valor invalido (Negativo ou naior que o estoque preexistente)')
-                            elif remo == 2:
-                                ind = int(input('Digite o índice do item que deseja remover: '))
-                                produtos.pop(ind-1)
-                                print('Item removido')
+                            pet.remover(produtos)
                         elif op == 2:
-                            ind = int(input('Digite o índice do item que deseja alterar: '))
-                            a = input('deseja alterar o nome ou o preço?[n/p] ')
-                            if a == 'n':
-                                alt = input('Digite o novo nome do item: ')
-                                produtos[ind-1][0] = alt
-                                print('Nome alterado')
-                            elif a == 'p':
-                                alt = input('Digite o novo valor do item: ')
-                                if '-' not in alt:
-                                    produtos[ind-1][1] = alt
-                                else:
-                                    print('Valor inválido (Não pode conter - no valor)')
+                            pet.alterar(produtos)
                         elif op == 3:
-                            print('1-Adicionar estoque de um item que já está no catálogo')
-                            print('2-Adicionar novo item')
-                            ad = int(input('Ação desejada: '))
-                            if ad == 1:
-                                ind = int(input('Digite o índice do item que deseja adicionar estoque: '))
-                                est = int(input('Quantidade que será a dicionada ao item: '))
-                                if est > 0:
-                                    produtos[ind-1][2] += est
-                                else:
-                                    print('Quantidade inválida')
-                            elif ad == 2:
-                                ad_n = input('Digite o nome do novo item: ')
-                                ad_p = input('Digite o preço do novo item: ')
-                                ad_e = int(input('Quanto será o estoque desse item: '))
-                                if len(ad_n.strip()) != 0 and len(ad_p.strip()) != 0 and ad_e >0:
-                                    produtos.append([ad_n,ad_p,ad_e])
-                                    print('Produto adicionado')
-                                else:
-                                    print('Valores inválidos')
+                            pet.adicionar(produtos)
                         elif op == 4:
-                            for i in produtos:
-                                print('----------')
-                                print(f'{ind}-{i[0]} {i[1]}')
-                                print(f'Estoque: {i[2]}')
-                                ind += 1
+                            pet.listar(produtos, 'produtos')
                 elif op == '2':
                     print('---Serviços disponíveis---')
                     ind = 0
